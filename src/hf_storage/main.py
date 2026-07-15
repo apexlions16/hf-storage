e03c9869ff002bdc8df832ac70dc51e604a299c1
apexlions16 <hcywashere@gmail.com>
2026-07-15 18:43:34 +0300

    chore: show version 0.1.1 in the Windows app

--- src/hf_storage/main.py
+++ src/hf_storage/main.py
@@ -1,3 +1,7 @@
-from .window import AppWindow, main
+from . import window as _window
+
+_window.VERSION = "0.1.1"
+AppWindow = _window.AppWindow
+main = _window.main
 
 __all__ = ["AppWindow", "main"]
